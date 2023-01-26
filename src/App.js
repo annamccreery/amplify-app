38d5f6012730c43791b0bf955ba81f3846840d26
Anna McCreery <amccreery@madisoncollege.edu>
2023-01-26 11:49:36 -0600

    error updates
    updated to new code requirements per help video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Authenticator>
+      {({ signout, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signout}>Sign Out</button>
+        </main>
+      )}
+    </Authenticator>;
+  )
 }
 
-export default App;
+export default (App)
